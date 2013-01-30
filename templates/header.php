d5961196157644aaac45b00226896cc7a81df197
Carl Martens <carlrmartens@gmail.com>
2013-01-29 22:30:02 -0600

    added in the bloginfo description to the header.php

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,14 @@
 <header class="banner" role="banner">
   <div class="container">
-    <a class="brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
+    <?php if (is_front_page()): ?>
+      <h1>
+        <a class="brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a> <small class="site-description"><?php bloginfo( 'description' ); ?></small>
+      </h1>
+      <?php else: ?>
+      <h2>
+        <a class="brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a> <small class="site-description"><?php bloginfo( 'description' ); ?></small>
+      </h2>
+    <?php endif; ?>
     <nav class="nav-main" role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
